f2792ce53853349f2cb3fe804be9b5b785c4723e
chahat <chahataggarwal28@gmail.com>
2017-02-04 15:54:55 +0530

    table

---
+++ js/table.js
@@ -0,0 +1,32 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class Table extends React.Component {
+  render() {
+    return <table class="mdl-data-table mdl-js-data-table mdl-data-table--selectable mdl-shadow--2dp">
+  <thead>
+    <tr>
+      <th class="mdl-data-table__cell--non-numeric">SUBJECT</th>
+      <th>BUNKS</th>
+      
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td class="mdl-data-table__cell--non-numeric">Acrylic (Transparent)</td>
+      <td>25</td>
+    </tr>
+    <tr>
+      <td class="mdl-data-table__cell--non-numeric">Plywood (Birch)</td>
+      <td>50</td>
+    </tr>
+    <tr>
+      <td class="mdl-data-table__cell--non-numeric">Laminate (Gold on Blue)</td>
+      <td>10</td>
+    </tr>
+  </tbody>
+</table>;
+  }
+}
+
+module.exports = Table
